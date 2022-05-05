252a2673f77d6012881f87782fabff2d8d1d21d0
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-05 21:50:36 +0100

    Complete code and tests

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -2,5 +2,25 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        first_card = Card("hearts", 1)
+        second_card = Card("diamonds", 3)
+        third_card = Card("diamonds", 10)
+        self.card_game = CardGame([first_card, second_card, third_card])
+
+    def test_check_for_ace(self):
+        card = self.card_game.cards[0]
+        result = self.card_game.check_for_ace(card)
+        self.assertEqual(True, result)
+
+    def test_highest_card(self):
+        first_card = self.card_game.cards[1]
+        second_card = self.card_game.cards[2]
+        highest_card = self.card_game.highest_card(first_card, second_card)
+        self.assertEqual(second_card, highest_card)
+
+    def test_cards_total(self):
+        cards = self.card_game.cards
+        total = self.card_game.cards_total(cards)
+        self.assertEqual("You have a total of 14", total)
